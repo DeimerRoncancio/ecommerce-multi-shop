4ba9c5bcfe31eb54d4a43e06265cd2429f0be6f8
DeimerRoncancio <deimerroncancio@gmail.com>
2025-01-06 20:35:16 -0500

    Estructura básica de pagina de login creada

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Route, Routes } from 'react-router'
-import Login from './pages/Login.tsx'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router";
+import Login from "./pages/Login.tsx";
+import App from "./App.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="login" element={<Login />} />
     </Routes>
   </BrowserRouter>,
-)
+);
