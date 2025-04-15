f88b3c1a12640a88c64bebee304dd91e451d3e42
DeimerRoncancio <deimerroncancio@gmail.com>
2025-04-14 22:12:59 -0500

    Footer terminado de estilizar

--- src/shared/components/navbar/NavBar.tsx
+++ src/shared/components/navbar/NavBar.tsx
@@ -5,10 +5,11 @@ import ProfileButton from "./ProfileButton";
 import Search from "./Search";
 import MenuButton from "./MenuButton";
 import useGetProducts from "../../hooks/api/useGetProducts";
-import { BsTelephone, BsTwitterX } from "react-icons/bs";
+import { BsCashStack, BsPaypal, BsShop, BsTelephone, BsTwitterX } from "react-icons/bs";
 import { TfiEmail } from "react-icons/tfi";
 import { SlLocationPin } from "react-icons/sl";
-import { FaFacebook, FaInstagram, FaLinkedin, FaTiktok, FaYoutube } from "react-icons/fa6";
+import { FaApple, FaFacebook, FaGoogle, FaInstagram, FaLinkedin, FaTiktok, FaYoutube } from "react-icons/fa6";
+import { IoCardOutline } from "react-icons/io5";
 
 export default function NavBar() {
   const { products } = useGetProducts();
@@ -65,41 +66,71 @@ export default function NavBar() {
               </p>
 
               <div className="flex items-center justify-start gap-2">
-                <SlLocationPin color="#f14a13" size={18} /> 
+                <SlLocationPin color="#f14a13" size={18} />
                 <p className="leading-4 text-[#636669]">
                   123 Fashion Street, New York, NY 10001
                 </p>
               </div>
               <div className="flex items-center justify-start gap-2">
-                <BsTelephone color="#f14a13" size={18} /> 
+                <BsTelephone color="#f14a13" size={18} />
                 <p className="leading-4 text-[#636669]">
                   +1 (555) 123-4567
                 </p>
               </div>
               <div className="flex items-center justify-start gap-2">
-                <TfiEmail color="#f14a13" size={18} /> 
+                <TfiEmail color="#f14a13" size={18} />
                 <p className="leading-4 text-[#636669]">
-                hello@example.com
+                  hello@example.com
                 </p>
               </div>
             </div>
           </div>
           <div className="text-[#636669]">
-            <h2 className="text-[#431d1a] font-medium text-lg mb-8">Productos</h2>
-            <ul>{products.map(item => <li className="mb-3">{item.productName}</li>)}</ul>
-          </div>
-          <div className="text-[#636669]">
-            <h2 className="text-[#431d1a] font-medium text-lg mb-8">Sobre nosotros</h2>
+            <h2 className="text-[#431d1a9c] font-medium text-lg mb-8">Productos</h2>
             <ul>
-              <li className="mb-3">Nosotros</li>
-              <li className="mb-3">Politica de calidad</li>
-              <li className="mb-3">Política de Garantías y Devoluciones</li>
-              <li className="mb-3">Política de tratamiento de datos</li>
-              <li className="mb-3">Contactenos</li>
+              {
+                products.map(item => (
+                  <li className="mb-3 hover:text-[#f04913] transition-all duration-300">
+                    <NavLink to="">
+                      {item.productName}
+                    </NavLink>
+                  </li>
+                ))
+              }
             </ul>
           </div>
           <div className="text-[#636669]">
-            <h2 className="text-[#431d1a] font-medium text-lg mb-8">Siguenos</h2>
+            <h2 className="text-[#431d1a9c] font-medium text-lg mb-8">Sobre nosotros</h2>
+            <ul>
+              <li className="mb-3 hover:text-[#f04913] transition-all duration-300">
+                <NavLink to="">
+                  Nosotros
+                </NavLink>
+              </li>
+              <li className="mb-3 hover:text-[#f04913] transition-all duration-300">
+                <NavLink to="">
+                  Politica de calidad
+                </NavLink>
+              </li>
+              <li className="mb-3 hover:text-[#f04913] transition-all duration-300">
+                <NavLink to="">
+                  Política de Garantías y Devoluciones
+                </NavLink>
+              </li>
+              <li className="mb-3 hover:text-[#f04913] transition-all duration-300">
+                <NavLink to="">
+                  Política de tratamiento de datos
+                </NavLink>
+              </li>
+              <li className="mb-3 hover:text-[#f04913] transition-all duration-300">
+                <NavLink to="">
+                  Contactenos
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+          <div className="text-[#636669]">
+            <h2 className="text-[#431d1a9c] font-medium text-lg mb-8">Siguenos</h2>
             <div className="flex gap-3">
               <NavLink to="" className="p-2 rounded-full bg-[#e8e9e9] text-[#212529] hover:bg-[#f04913] 
               hover:text-white transition-all duration-300">
@@ -128,15 +159,34 @@ export default function NavBar() {
             </div>
           </div>
         </div>
-        <div className="flex flex-col items-center">
-          <p>Aceptamos medios de pago:</p>
-          <div className="flex">
-            <p>Terminos de servicio</p>
-            <p>Terminos de servicio</p>
-            <p>Terminos de servicio</p>
+        <div className="flex flex-col items-center py-7 bg-[#fff4ef]">
+          <div className="flex items-center gap-3 text-[#5f6368]">
+            <p className="font-semibold text-base">Aceptamos:</p>
+            <IoCardOutline size={24} className="hover:text-[#f14a13] transition-all duration-300" />
+            <BsPaypal size={24} className="hover:text-[#f14a13] transition-all duration-300" />
+            <FaApple size={24} className="hover:text-[#f14a13] transition-all duration-300" />
+            <FaGoogle size={24} className="hover:text-[#f14a13] transition-all duration-300" />
+            <BsShop size={24} className="hover:text-[#f14a13] transition-all duration-300" />
+            <BsCashStack size={24} className="hover:text-[#f14a13] transition-all duration-300" />
           </div>
-          <p>© Copyright Multi Shop. Todos los derechos reservados</p>
-          <p>Desarrollado por Deimer Roncancio</p>
+          <div className="flex gap-4 text-sm text-[#7a8091] my-6">
+            <NavLink to="" className="hover:text-[#f14a13] transition-all duration-300">
+              Terminos de servicio
+            </NavLink>
+            <NavLink to="" className="hover:text-[#f14a13] transition-all duration-300">
+              Politica de privacidad
+            </NavLink>
+            <NavLink to="" className="hover:text-[#f14a13] transition-all duration-300">
+              Politicas de Cookies
+            </NavLink>
+          </div>
+          <p>© Copyright <b className="text-[#36393e]">MultiShop</b>. Todos los derechos reservados</p>
+          <p className="flex gap-1">
+            Desarrollado por
+            <NavLink to="" className="text-[#f14a13] hover:text-[#f14b1381] transition-all duration-300">
+              Deimer Roncancio
+            </NavLink>
+          </p>
         </div>
       </footer>
     </>
