3c4535a8feaa3eaed81e4bb66ac66e057461301f
DeimerRoncancio <deimerroncancio@gmail.com>
2025-04-21 17:54:42 -0500

    Botón para añadir productos desde lista de deseos implementado

--- src/wishlist/pages/WishList.tsx
+++ src/wishlist/pages/WishList.tsx
@@ -1,14 +1,31 @@
+import useCart from "../../cart/hooks/useCart";
+import useGetProducts from "../../shared/hooks/api/useGetProducts";
 import WishListItem from "../components/WishListItem";
 import useWishList from "../hooks/useWishList";
 
 export default function WishList() {
+  const { items, handleAddItem } = useCart();
+  const { products } = useGetProducts();
   const { wishList } = useWishList();
+
+  const handleAddToWishList = () => {
+    const cartIds = new Set(items.map(listItem => listItem.id));
+    const itemsToAdd = wishList.filter(({ id }) => !cartIds.has(id));
+    const productsById = new Map(products.map(product => [product.id, product]));
+
+    itemsToAdd.forEach(({ id }) => {
+      const product = productsById.get(id);
+      product && handleAddItem(product);
+    })
+  }
 
   return (
     <>
       <div className="flex justify-between items-center mb-4">
         <h2 className="text-2xl text-[#5e472d]">Lista de deseos</h2>
-        <button className="btn">Agregar el carrito</button>
+        <button className="btn"
+          onClick={() => handleAddToWishList()}
+        >Agregar el carrito</button>
       </div>
       <div>
         <ul className="flex flex-wrap gap-6">
