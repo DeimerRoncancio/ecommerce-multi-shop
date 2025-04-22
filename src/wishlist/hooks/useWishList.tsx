18f02c3915e0937f10ed343e665a271582c7946e
DeimerRoncancio <deimerroncancio@gmail.com>
2025-04-22 13:02:01 -0500

    Lógica de agregado de múltiples productos a carrito desde lista de deseos transportada a hook "useWishList"

--- src/wishlist/hooks/useWishList.tsx
+++ src/wishlist/hooks/useWishList.tsx
@@ -1,24 +1,49 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { ProductTypes } from "../../products/types/product";
 import { useWishListStorage } from "../storage/useWishListStorage";
 import { productToWishList } from "../mappers/wishlist-mapper";
+import useCart from "../../cart/hooks/useCart";
+import useGetProducts from "../../shared/hooks/api/useGetProducts";
 
 export default function useWishList() {
   const { wishList, addWishListItem, removeWishListItem, setWishListFromStorage } = useWishListStorage();
+  const [ itemsInCart, setItemInCart ] = useState(0);
+  const { items, handleAddItem } = useCart();
+  const { products } = useGetProducts();
 
   const handleAddWishListItem = (product: ProductTypes) => {
     const wishListItem = productToWishList({ product });
-
     addWishListItem(wishListItem);
   }
 
   const handleRemoveWishListItem = (id: string) => {
     removeWishListItem(id);
   }
+  
+  const handleAddToCartSinceWishList = () => {
+    const itemsToAdd = getItemsToAdd();
+    const productsById = new Map(products.map(product => [product.id, product]));
+    
+    setItemInCart(itemsToAdd.length);
+    itemsToAdd.forEach(({ id }) => {
+      const product = productsById.get(id);
+      product && handleAddItem(product);
+    })
+  }
+
+  const getItemsToAdd = () => {
+    const cartIds = new Set(items.map(listItem => listItem.id));
+    return wishList.filter(({ id }) => !cartIds.has(id));
+  }
 
   const loadWishListFromStorage =() => {
     if (!wishList.length) setWishListFromStorage();
   }
+
+  useEffect(()=>{
+    const itemsToAdd = getItemsToAdd();
+    setItemInCart(itemsToAdd.length);
+  }, [items])
 
   useEffect(() => {
     loadWishListFromStorage();
@@ -26,8 +51,10 @@ export default function useWishList() {
 
   return {
     wishList,
+    itemsInCart,
     handleAddWishListItem,
     handleRemoveWishListItem,
+    handleAddToCartSinceWishList,
     loadWishListFromStorage
   }
 }
--- src/wishlist/pages/WishList.tsx
+++ src/wishlist/pages/WishList.tsx
@@ -1,46 +1,21 @@
-import { useEffect, useState } from "react";
-import useCart from "../../cart/hooks/useCart";
-import useGetProducts from "../../shared/hooks/api/useGetProducts";
 import WishListItem from "../components/WishListItem";
 import useWishList from "../hooks/useWishList";
 
 export default function WishList() {
-  const { items, handleAddItem } = useCart();
-  const [ itemsInCart, setItemInCart ] = useState(0);
-  const { products } = useGetProducts();
-  const { wishList } = useWishList();
-
-  const getItemsToAdd = () => {
-    const cartIds = new Set(items.map(listItem => listItem.id));
-    return wishList.filter(({ id }) => !cartIds.has(id));
-  }
-
-  const handleAddToWishList = () => {
-    const itemsToAdd = getItemsToAdd();
-    const productsById = new Map(products.map(product => [product.id, product]));
-
-    setItemInCart(itemsToAdd.length);
-    itemsToAdd.forEach(({ id }) => {
-      const product = productsById.get(id);
-      product && handleAddItem(product);
-    })
-  }
-
-  useEffect(()=>{
-    const itemsToAdd = getItemsToAdd();
-    setItemInCart(itemsToAdd.length);
-  }, [items])
+  const { wishList, itemsInCart, handleAddToCartSinceWishList } = useWishList();
 
   return (
     <>
       <div className="flex justify-between items-center mb-4">
         <h2 className="text-2xl text-[#5e472d]">Lista de deseos</h2>
         <button className={`btn ${itemsInCart ? 'btn-neutral btn-outline' : 'btn-disabled text-[#a08f88]'}`}
-        onClick={handleAddToWishList}>
+        onClick={handleAddToCartSinceWishList}>
           {
-            itemsInCart
-            ? 'Agregar el carrito'
-            : 'Productos agregados'
+            !wishList.length
+            ? 'No hay productos'
+            : itemsInCart
+              ? 'Agregar al carrito'
+              : 'Productos agregados'
           }
         </button>
       </div>
