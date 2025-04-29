cffb1b14f80ccfb6a02c4277f4162408f0da6b54
DeimerRoncancio <deimerroncancio@gmail.com>
2025-04-28 23:50:23 -0500

    Sistema de modal de categorías creado

--- src/shared/components/navbar/categories/Categories.tsx
+++ src/shared/components/navbar/categories/Categories.tsx
@@ -2,9 +2,12 @@ import { useEffect, useState } from "react"
 import { CategoriesType } from "../../../../products/types/categories";
 import { mapApiToCategories } from "../../../../products/mappers/categories-mapper";
 import { getCategories } from "../../../../products/services/api/categories";
+import useProducts from "../../../../products/hooks/api/useProducts";
 
 export default function Categories() {
   const [categories, setCategories] = useState<CategoriesType[]>([]);
+  const [isModalVisible, setModalVisible] = useState(false);
+  const { products } = useProducts();
 
   useEffect(() => {
     getCategories()
@@ -14,17 +17,32 @@ export default function Categories() {
 
   return (
     <div className="text-[#343e49] text-sm">
-      <ul className="flex justify-center items-center border-b-[1px] border-[#f1e1dc] gap-10">
+      <ul className="flex justify-center items-center border-b-[1px] border-[#f1e1dc]">
         {
           categories.map(cat => (
             <li key={cat.id}>
-              <button className="hover:text-black cursor-pointer py-4 hover:underline decoration-1">
+              <button className="hover:text-black cursor-pointer py-4 hover:underline decoration-1 px-5"
+                onMouseEnter={() => setModalVisible(true)}
+                onMouseLeave={() => setModalVisible(false)}
+              >
                 <p>{cat.name}</p>
               </button>
             </li>
           ))
         }
       </ul>
+      <div className={`${!isModalVisible && 'hidden'} ajust-width`}
+        onMouseEnter={() => setModalVisible(true)}
+        onMouseLeave={() => setModalVisible(false)}
+      >
+        <ul>
+          {
+            products.map(product => (
+              <li key={product.id}>{product.name}</li>
+            ))
+          }
+        </ul>
+      </div>
     </div>
   )
 }
