929e2b40181269bdb26bc3e631c71a1a1c5c5fa1
DeimerRoncancio <deimerroncancio@gmail.com>
2025-06-01 22:33:12 -0500

    Hook "useUpdateUser", para actualizar datos de usuario, creado

---
+++ src/profile/hooks/api/useUpdateUser.tsx
@@ -0,0 +1,36 @@
+import { initialUserValues } from "../../helpers/users-initial-values.helper";
+import { UpdateRequestTypes, UserTypes, UserUpdateTypes } from "../../types/user";
+import { updateTypesToRequestTypes } from "../../mappers/profile.mapper";
+import { updateDataUser } from "../../services/users.api";
+import { useForm, useWatch } from "react-hook-form";
+import { useEffect } from "react";
+
+type UseUpdateUserProps = {
+  user: UserTypes,
+  updateUser: (user: UpdateRequestTypes) => void
+}
+
+export const useUpdateUser = ({ user, updateUser }: UseUpdateUserProps) => {
+  const { register, handleSubmit, reset, control } = useForm<UserUpdateTypes>();
+  const userInitialValues = initialUserValues(user);
+  const currentValues = useWatch({ control });
+
+  const sendData = (data: UserUpdateTypes) => {
+    const userInfo: UpdateRequestTypes = updateTypesToRequestTypes(data);
+    updateDataUser(user.id, userInfo);
+    updateUser(userInfo);
+  }
+
+  useEffect(() => {
+    if (user) reset(userInitialValues);
+  }, [user, reset]);
+
+
+  return {
+    userInitialValues,
+    currentValues,
+    sendData,
+    register,
+    handleSubmit
+  }
+}
--- src/profile/pages/Profile.tsx
+++ src/profile/pages/Profile.tsx
@@ -2,11 +2,9 @@ import { useOutletContext } from "react-router"
 import { UpdateRequestTypes, UserTypes, UserUpdateTypes } from "../types/user";
 import UserDataField from "../components/UserDataField";
 import UserDataRadio from "../components/UserDataRadio";
-import { SubmitHandler, useForm, useWatch } from "react-hook-form";
+import { SubmitHandler } from "react-hook-form";
 import { useEffect, useRef, useState } from "react";
-import { updateTypesToRequestTypes } from "../mappers/profile.mapper";
-import { initialUserValues } from "../helpers/users-initial-values.helper";
-import { updateDataUser } from "../services/users.api";
+import { useUpdateUser } from "../hooks/api/useUpdateUser";
 
 type userContext = {
   user: UserTypes,
@@ -16,28 +14,21 @@ type userContext = {
 
 export default function Profile() {
   const { user, userLoading: loading, updateUser } = useOutletContext<userContext>();
-  const { register, handleSubmit, reset, control } = useForm<UserUpdateTypes>();
-  const [isActive, setIsActive] = useState(false);
-  const [isUpdated, setIsUpdated] = useState(false);
-  const userInitialValues = initialUserValues(user);
-  const currentValues = useWatch({ control });
+  const {
+    userInitialValues,
+    currentValues,
+    sendData, register,
+    handleSubmit 
+  } = useUpdateUser({ user, updateUser });
   const timeoutRef = useRef<ReturnType<typeof setTimeout> | null>(null);
+  const [showAlert, setShowAlert] = useState(false);
+  const [isActive, setIsActive] = useState(false);
 
   const onSubmit: SubmitHandler<UserUpdateTypes> = (data) => {
-    const userInfo: UpdateRequestTypes = updateTypesToRequestTypes(data);
-
-    updateDataUser(user.id, userInfo);
-    updateUser(userInfo);
-
+    sendData(data);
+    setShowAlert(true);
     setIsActive(false);
-    setIsUpdated(true);
-
     handleUpdatedAlert();
-  }
-
-  const handleUpdatedAlert = () => {
-    if (isUpdated && timeoutRef.current !== null) clearTimeout(timeoutRef.current);
-    timeoutRef.current = setTimeout(() => setIsUpdated(false), 4000);
   }
 
   const handleActive = () => {
@@ -47,9 +38,10 @@ export default function Profile() {
     setIsActive(isChanged);
   }
 
-  useEffect(() => {
-    if (user) reset(userInitialValues);
-  }, [user, reset]);
+  const handleUpdatedAlert = () => {
+    if (showAlert && timeoutRef.current !== null) clearTimeout(timeoutRef.current);
+    timeoutRef.current = setTimeout(() => setShowAlert(false), 4000);
+  }
 
   useEffect(() => handleActive(), [currentValues]);
 
@@ -97,13 +89,13 @@ export default function Profile() {
       }
 
       <div role="alert" className={`alert alert-success fixed bottom-5 right-5 transition-all duration-300
-      ${isUpdated ? 'opacity-100 visible -translate-x-0' : 'opacity-0 invisible translate-x-full'}`}>
+      ${showAlert ? 'opacity-100 visible -translate-x-0' : 'opacity-0 invisible translate-x-full'}`}>
         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 shrink-0 stroke-current" fill="none" viewBox="0 0 24 24">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
         </svg>
         <span>Tus datos han sido actualizados!</span>
         <button className="btn btn-sm bg-[#186c56] border-none text-[#053b2d] shadow-none rounded-full p-0 m-0 h-fit
-        ml-10" onClick={() => setIsUpdated(false)}>
+        ml-10" onClick={() => setShowAlert(false)}>
           <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 shrink-0 stroke-current" fill="none" viewBox="0 0 24 24">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
           </svg>
