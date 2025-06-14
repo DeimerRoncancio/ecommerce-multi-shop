d8a9706d4aa8df701e1611ef6d48c4452ac677ed
DeimerRoncancio <deimerroncancio@gmail.com>
2025-06-13 22:49:36 -0500

    Estructura de rutas cambiada

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,39 @@
 import {
   type RouteConfig,
+  index,
+  layout,
+  prefix,
   route,
 } from "@react-router/dev/routes";
 
 export default [
-  // * matches all URLs, the ? makes it optional so it will match / as well
-  route("*?", "catchall.tsx"),
+  // App
+  layout('App.tsx', [
+    // Home
+    index('home/pages/Home.tsx'),
+    
+    // Profile
+    ...prefix('profile', [
+      layout('profile/pages/ProfilePage.tsx', [
+        //Auth Profile
+        layout('auth/routes/ProtectedAuth.tsx', [
+          index('profile/pages/Profile.tsx')
+        ]),
+        route('wish-list', 'wishlist/pages/WishList.tsx'),
+      ]),
+    ]),
+  ]),
+  
+  // Cart
+  ...prefix('cart', [
+    layout('cart/pages/Cart.tsx', [
+      layout('cart/components/CartHeader.tsx', [
+        index('cart/components/CartContent.tsx')
+      ])
+    ])
+  ]),
+
+  // Auth
+  route('login', 'auth/pages/Login.tsx'),
+  route('register', 'auth/pages/register.tsx'),
 ] satisfies RouteConfig;
