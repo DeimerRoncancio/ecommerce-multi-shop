914f347a121c846fedc996a9fad8e7dae3a9b032
DeimerRoncancio <deimerroncancio@gmail.com>
2025-06-26 16:48:31 -0500

    Botón de modificación de foto de perfil creado más pequeños cambios de estilo en foto de perfil

--- src/profile/components/UserDataField.tsx
+++ src/profile/components/UserDataField.tsx
@@ -11,14 +11,12 @@ export default function UserDataField({ register, name, fieldName }: UserDataFie
   return (
     <div>
       <span className="text-[#c7c7c7]">{fieldName}</span>
-      {/* <div className="p-3 pl-4 mt-3 border-[1px] border-[#ebebeb] rounded-xl"> */}
-        <input
-          type="text"
-          className="p-3 pl-4 mt-3 border-[1px] border-[#ebebeb] rounded-xl outline-0 w-full focus:outline-2 
+      <input
+        type="text"
+        className="p-3 pl-4 mt-3 border-[1px] border-[#ebebeb] rounded-xl outline-0 w-full focus:outline-2 
           focus:outline-[#ffc1ad] focus:border-[#f14913]"
-          {...register(name)}
-        />
-      {/* </div> */}
+        {...register(name)}
+      />
     </div>
   )
 }
--- src/profile/layouts/profile-layout.tsx
+++ src/profile/layouts/profile-layout.tsx
@@ -6,6 +6,7 @@ import Breadcrumb from "../components/Breadcrumb";
 import type { Route } from "./+types/profile-layout";
 import { getSession } from "../../sessions.server";
 import LogoutButton from "../components/LogoutButton";
+import { RiImageEditLine } from "react-icons/ri";
 
 export async function loader({ request }: Route.LoaderArgs) {
   const session = await getSession(request.headers.get('Cookie'));
@@ -32,8 +33,16 @@ export default function ProfileLayout({ loaderData }: Route.ComponentProps) {
         <div className="flex flex-col w-[258px] p-5 pt-5 rounded-3xl">
           <div className="flex flex-col items-center gap-3">
             <div className="avatar">
-              <div className="w-24 rounded-full">
+              <div className="w-28 rounded-full border-4 border-white shadow-[0px_7px_29px_0px_rgba(100,100,111,0.2)]">
                 <AvatarImage loading={loading} user={user} />
+                {
+                  user.name.length &&
+                    <button className="absolute rounded-full top-1 text-transparent hover:text-gray-200 
+                    hover:bg-[#16161644] w-[104px] h-[104px] transition-all duration-100 outline-0 flex 
+                    justify-center items-center cursor-pointer">
+                      <RiImageEditLine className="transition-all duration-100" size={28} />
+                    </button>
+                }
               </div>
             </div>
             <h1 className="text-lg text-[#5e472d] font-semibold">
