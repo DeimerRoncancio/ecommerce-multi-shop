921b77db139c1b242f3dec2bf94e776644092e00
DeimerRoncancio <deimerroncancio@gmail.com>
2025-06-27 22:23:18 -0500

    Prqueño cambio de diseño y estructura de código en modal de editación de imagen de perfil

--- src/profile/components/profile-edit-modal/EditImageModal.tsx
+++ src/profile/components/profile-edit-modal/EditImageModal.tsx
@@ -32,84 +32,75 @@ export default function EditImageModal({ showModal, onClose }: EditImageModalPro
   }
 
   return (
-    <div
-      className={`${showModal ? 'visible opacity-100' : 'invisible opacity-0'}  transition-all duration-100 w-screen
-      h-full z-20 fixed top-0 flex justify-center items-center`}
-    >
+    <div className={`${showModal ? 'visible opacity-100' : 'invisible opacity-0'}  transition-all duration-100 w-screen
+    h-full z-20 fixed top-0 flex justify-center items-center`}>
       <div className="bg-[#1c1c1c7c] w-full h-full absolute" onClick={() => {
         onClose();
         setPreviewImage(null);
       }} />
-      <div
-        className={`grid grid-rows-[auto_1fr_auto] z-20 bg-white w-[560px] text-[#212529] h-[calc(100%-110px)]
-        min-h-[164px] max-h-[853px] rounded-3xl  ${!showModal && 'scale-105'} transition-all duration-150`}
-      >
-        <div className="p-3 pl-4 text-xl font-medium border-b border-[#ebebeb]">
-          <h1>Personaliza tu foto</h1>
-        </div>
-        <div className="flex justify-center items-center">
-          {
-            previewImage == null
-              ? (
-                <label
-                  onDrop={handleDrop}
-                  onDragEnter={() => {
-                    setIsDragOver(true);
-                  }}
-                  onDragLeave={() => {
-                    setIsDragOver(false);
-                  }}
-                  onDragOver={(e: React.DragEvent) => {
-                    e.preventDefault()
-                    setIsDragOver(true);
-                  }}
-                  className={`w-72 h-60 border-2 border-dashed hover:bg-[#f9fafb] rounded-2xl cursor-pointer transition-all
-                duration-300
-                ${isDragOver
-                      ? "border-blue-500 bg-blue-50 scale-105"
-                      : "border-gray-300 hover:border-gray-400 hover:bg-gray-50"}
-                  ${isUploading && "pointer-events-none opacity-75"}`}
-                >
-                  <input type="file" className="hidden" onChange={handleChangeImage} />
-                  <div className="flex flex-col h-full items-center justify-center space-y-1">
-                    <div
-                      className={`w-16 h-16 rounded-full flex items-center justify-center transition-colors
-                    duration-200 ${isDragOver ? "bg-blue-100" : "bg-gray-100"}`}
-                    >
-                      {isUploading ? (
-                        <div className="w-8 h-8 border-2 border-blue-500 border-t-transparent rounded-full animate-spin" />
-                      ) : (
-                        <BiUpload
-                          className={`w-8 h-8 transition-colors duration-200 
-                        ${isDragOver ? "text-blue-500" : "text-gray-500"}`}
-                        />
-                      )}
+      <div className={`z-20 bg-white w-[560px] text-[#212529] h-[calc(100%-110px)] min-h-[164px] max-h-[853px]
+      rounded-3xl  ${!showModal && 'scale-105'} transition-all duration-150`}>
+        <form className="grid grid-rows-[auto_1fr_auto] h-full">
+          <div className="p-3 pl-4 text-xl font-medium border-b border-[#ebebeb]">
+            <h1>Personaliza tu foto</h1>
+          </div>
+          <div className="flex justify-center items-center">
+            {
+              previewImage == null
+                ? (
+                  <label onDrop={handleDrop} onDragEnter={() => setIsDragOver(true)} onDragLeave={() => setIsDragOver(false)}
+                    className={`w-72 h-60 border-2 border-dashed hover:bg-[#f9fafb] rounded-2xl cursor-pointer transition-all
+                  duration-300 ${isUploading && "pointer-events-none opacity-75"}
+                  ${isDragOver
+                        ? "border-[#ec5320] bg-[#fff4f0] scale-105"
+                        : "border-gray-300 hover:border-gray-400 hover:bg-gray-50"}`}
+                    onDragOver={(e: React.DragEvent) => {
+                      e.preventDefault()
+                      setIsDragOver(true);
+                    }}>
+                    <input
+                      type="file"
+                      className="hidden"
+                      onChange={handleChangeImage}
+                    />
+                    <div className="flex flex-col h-full items-center justify-center space-y-1">
+                      <div className={`w-16 h-16 rounded-full flex items-center justify-center transition-colors
+                      duration-200 ${isDragOver ? "bg-[#ffe7df]" : "bg-gray-100"}`}>
+                        {isUploading ? (
+                          <div className="w-8 h-8 border-2 border-[#ec5320] border-t-transparent rounded-full animate-spin" />
+                        ) : (
+                          <BiUpload
+                            className={`w-8 h-8 transition-colors duration-200 
+                        ${isDragOver ? "text-[#ec5320]" : "text-gray-500"}`}
+                          />
+                        )}
+                      </div>
+                      <div className="space-y-2 text-center">
+                        <h3 className="text-lg font-semibold text-gray-700">
+                          {isUploading ? "Subiendo imágenes..." : "Arrastra tus imágenes aquí"}
+                        </h3>
+                        <p className="text-sm text-gray-500">o haz clic para seleccionar archivos</p>
+                        <p className="text-xs text-gray-400">PNG, JPG, GIF, WEBP</p>
+                      </div>
                     </div>
-                    <div className="space-y-2 text-center">
-                      <h3 className="text-lg font-semibold text-gray-700">
-                        {isUploading ? "Subiendo imágenes..." : "Arrastra tus imágenes aquí"}
-                      </h3>
-                      <p className="text-sm text-gray-500">o haz clic para seleccionar archivos</p>
-                      <p className="text-xs text-gray-400">PNG, JPG, GIF, WEBP</p>
+                  </label>
+                ) : (
+                  <div className="avatar">
+                    <div className="w-80 rounded-full bg-black border-4 border-[#f9761a] shadow-[0_0_25px_3px_#ffc69e]">
+                      <img src={previewImage} />
                     </div>
                   </div>
-                </label>
-              ) : (
-                <div className="avatar">
-                  <div className="w-80 rounded-full border-4 bg-black border-white outline-2 outline-[#ec5320]">
-                    <img src={previewImage} />
-                  </div>
-                </div>
-              )
-          }
-        </div>
-        <div className="p-4 text-xl flex justify-end font-medium border-t gap-4 border-[#ebebeb]">
-          <button className="btn rounded-full" onClick={() => {
-            onClose();
-            setPreviewImage(null);
-          }}>Cancelar</button>
-          <button className="btn btn-neutral rounded-full">Guardar</button>
-        </div>
+                )
+            }
+          </div>
+          <div className="p-4 text-xl flex justify-end font-medium border-t gap-4 border-[#ebebeb]">
+            <button className="btn rounded-full" onClick={() => {
+              onClose();
+              setPreviewImage(null);
+            }}>Cancelar</button>
+            <button type="submit" className="btn btn-neutral rounded-full">Guardar</button>
+          </div>
+        </form>
       </div>
     </div>
   )
