06292a3c182cbe3ead44393af58d26d03ab74919
DeimerRoncancio <deimerroncancio@gmail.com>
2025-08-06 16:15:54 -0500

    Product captured in single product page

--- src/products/routes/single-page-product.tsx
+++ src/products/routes/single-page-product.tsx
@@ -1,17 +1,36 @@
+import { getProduct } from "../services/products.api";
 import { Route } from "./+types/single-page-product";
 
 export async function loader({ params }: Route.LoaderArgs) {
-  return params;
+  const product = await getProduct(params.id);
+  return { product };
 }
 
 export default function SinglePageProduct({ loaderData }: Route.ComponentProps) {
-  const { id } = loaderData;
-  
+  const { product } = loaderData;
+
   return (
     <div>
       <h2>
-        {id}
+        {product.productName}
       </h2>
+      <h2>
+        {product.description}
+      </h2>
+      <h2>
+        {product.price}
+      </h2>
+      <ul>
+        {
+          product.categories.map(cat => <li>{cat.categoryName}</li>)
+        }
+      </ul>
+      <div>
+        {
+          product.productImages.map(img => <img src={`${img.imageUrl}`} />)
+        }
+      </div>
+      
     </div>
   )
 }
--- src/products/services/products.api.ts
+++ src/products/services/products.api.ts
@@ -4,3 +4,7 @@ import { products } from "../api/productsApi"
 export const getProducts = (): Promise<ProductsFromApiType[]> => {
   return products.get('').then(res => res.data.content);
 }
+
+export const getProduct = (id: string): Promise<ProductsFromApiType> => {
+  return products.get(`/${id}`).then(res => res.data);
+}
